638d91ca83c61a865a0eaeaf48a6cd4414bf00fb
ABITO <33889084+abitofdots@users.noreply.github.com>
2023-12-30 19:26:21 +0900

    Update wateringMate.sol

--- contracts/wateringMate.sol
+++ contracts/wateringMate.sol
@@ -7,31 +7,53 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract WateringMate is ERC20, Ownable {
     IERC721 public immutable nftToken;
+    uint256 public constant FT_PER_NFT = 1000000000000000000;
+    uint256 public totalDepositedNfts = 0;
+    uint256 public maxNftsPerTx; // 수정 가능한 변수로 변경
 
-    mapping(uint256 => address) public nftDepositors;
-    uint256 public constant MATE_PER_NFT = 1000000000000000000;
+    uint256[] private nftIds;
 
-    constructor(address _nftTokenAddress)
+    constructor(address _nftTokenAddress, uint256 _maxNftsPerTx)
         ERC20("Watering Mate", "~MATE")
         Ownable(msg.sender)
     {
         nftToken = IERC721(_nftTokenAddress);
+        maxNftsPerTx = _maxNftsPerTx;
     }
 
-    function depositNFT(uint256 nftId) external {
+    function depositNFTs(uint256[] calldata nftIdsToDeposit) external {
+        require(nftIdsToDeposit.length <= maxNftsPerTx, "Too many NFTs");
         address expectedNFTAddress = 0xE47E90C58F8336A2f24Bcd9bCB530e2e02E1E8ae;
-        require(nftToken.ownerOf(nftId) == msg.sender, "You must own the NFT");
-        require(address(nftToken) == expectedNFTAddress, "This NFT is not supported");
-        nftToken.transferFrom(msg.sender, address(this), nftId);
-        nftDepositors[nftId] = msg.sender;
-        _mint(msg.sender, MATE_PER_NFT);
+        for (uint256 i = 0; i < nftIdsToDeposit.length; i++) {
+            uint256 nftId = nftIdsToDeposit[i];
+            require(nftToken.ownerOf(nftId) == msg.sender, "You must own the NFT");
+            require(address(nftToken) == expectedNFTAddress, "This NFT is not supported");
+            nftToken.transferFrom(msg.sender, address(this), nftId);
+            nftIds.push(nftId);
+        }
+        _mint(msg.sender, FT_PER_NFT * nftIdsToDeposit.length);
+        totalDepositedNfts += nftIdsToDeposit.length;
+        
     }
 
-    function withdrawNFT(uint256 nftId) external {
-        require(nftDepositors[nftId] == msg.sender, "You are not the depositor");
-        require(balanceOf(msg.sender) >= MATE_PER_NFT, "Insufficient MATE balance");
-        _burn(msg.sender, MATE_PER_NFT);
-        nftToken.transferFrom(address(this), msg.sender, nftId);
-        delete nftDepositors[nftId];
+    function withdrawNFTs(uint256 numberOfNfts) external {
+        require(numberOfNfts <= maxNftsPerTx, "Too many NFTs");
+        require(numberOfNfts > 0, "Number of NFTs must be greater than 0");
+        require(nftIds.length >= numberOfNfts, "Not enough NFTs available to withdraw");
+        require(balanceOf(msg.sender) >= FT_PER_NFT * numberOfNfts, "Insufficient ~MATE balance");
+
+        for (uint256 i = 0; i < numberOfNfts; i++) {
+            uint256 lastIndex = nftIds.length - 1;
+            uint256 nftId = nftIds[lastIndex];
+            nftIds.pop();
+            nftToken.transferFrom(address(this), msg.sender, nftId);
+        }
+        _burn(msg.sender, FT_PER_NFT * numberOfNfts);
+        totalDepositedNfts -= numberOfNfts;
+    }
+
+    // 오너만이 maxNftsPerTx 값을 수정할 수 있는 함수
+    function setMaxNftsPerTx(uint256 _maxNftsPerTx) external onlyOwner {
+        maxNftsPerTx = _maxNftsPerTx;
     }
 }
